1b16facfe68325c7d9b9f6c802c6c05773c7eff3
tej.boolakee <tej.boolakee33@gmail.com>
2026-04-13 16:48:46 +0400

    feat: composant button

--- nexcart/packages/nexcart-design-system/src/story/Button/Button.stories.tsx
+++ nexcart/packages/nexcart-design-system/src/story/Button/Button.stories.tsx
@@ -1,5 +1,5 @@
-import type {Meta, StoryObj} from "@storybook/react";
-import {Button} from "../../components/Button/Button";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "../../components/Button/Button";
 
 const meta = {
     title: "Components/Button",
