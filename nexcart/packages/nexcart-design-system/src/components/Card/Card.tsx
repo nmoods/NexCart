e77e5019f4f9a740c0e7de4f4b914f995b2ec896
neehal.moodhoo <neehal.moodhoo@accenture.com>
2026-04-22 13:12:25 +0400

    feat(ui): Adding the proper css to the Card component

--- nexcart/packages/nexcart-design-system/src/components/Card/Card.tsx
+++ nexcart/packages/nexcart-design-system/src/components/Card/Card.tsx
@@ -12,17 +12,20 @@ interface CardProps {
 const Card = ({image, title, priceDescription, price, promo}: CardProps) => {
 
     return (
-        <div className={styles['nx-card--container']}>
+        <div className={styles['nx-card__container']}>
             <div className={styles['nx-card--image-container']}>
                 <div className={styles['nx-card--image']}>{image}</div>
                 {promo && <div className={styles['nx-card--promo']}>{promo}</div>}
             </div>
-            <div className={styles['nx-card--textblock']}>
-                <div className={styles['nx-card--title']}>{title}</div>
-                <div className={styles['nx-card--price-description']}>{priceDescription}</div>
-            </div>
-            <div className={styles['nx-card--price-container']}>
-                <span className={styles['nx-card--price']}>${price}</span>
+            <div className={styles['nx-card--info-container']}>
+                <div className={styles['nx-card--textblock']}>
+                    <div className={styles['nx-card--title']}>{title}</div>
+                    <div className={styles['nx-card--price-description']}>{priceDescription}</div>
+                </div>
+                <div className={styles['nx-card--divider']} />
+                <div className={styles['nx-card--price-container']}>
+                    <span className={styles['nx-card--price']}>${price}</span>
+                </div>
             </div>
         </div>
     );
