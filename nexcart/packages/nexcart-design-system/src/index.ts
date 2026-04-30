68ef8e3be544115f3b6afcdc7dc6741fd61bdf59
nmoods <98906999+nmoods@users.noreply.github.com>
2026-04-30 09:55:12 +0400

    Creation Admin Dashboard (#21)
    * feat(ui): Ajout du composant Pagination avec les stories et les tests (#18)
    
    Co-authored-by: neehal.moodhoo <neehal.moodhoo@accenture.com>
    
    * feat(ui): Ajout du nav de admin page
    
    * feat(ui): Adding admin ui
    
    ---------
    
    Co-authored-by: neehal.moodhoo <neehal.moodhoo@accenture.com>

--- nexcart/packages/nexcart-design-system/src/index.ts
+++ nexcart/packages/nexcart-design-system/src/index.ts
@@ -1,7 +1,7 @@
 export { Button } from './components/Button/Button';
 export { Dropdown } from './components/Dropdown/Dropdown';
 export { Input } from './components/Input/Input';
-export { Card } from './components/Card/Card';
+export { default as Card } from './components/Card/Card';
 export { Pagination } from './components/Pagination/Pagination';
 export { ProductHeader } from './components/ProductHeader/ProductHeader';
 export { ProductImageCard } from './components/ProductImageCard/ProductImageCard';
